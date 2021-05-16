d4fe987dd9ab9bab5d73b3e9089d6147ab850888
Juggernaut98 <andresdaville@gmail.com>
2021-05-16 11:58:06 -0700

    second project

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import logo from "../Assets/logo.svg";
+const navbar = () => {
+    return (
+        <div className="w-full bg-white border-b border-gray-200">
+            <div className="max-w-7x1 mx-auto">
+                <div className="flex justify-between items-center py-3">
+                    <div className="flex flex-1 justify-start">
+                       
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default navbar;
